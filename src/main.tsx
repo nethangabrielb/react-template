8b56595e1353b01e34a5fe4f360aaa8f3d68daa2
nethangabrielb <112095627+suix0@users.noreply.github.com>
2025-08-19 14:08:17 +0800

    feat: added switching of dark and light mode and system default

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "sonner";
+import { ThemeProvider } from "@/components/ThemeProvider.tsx";
 
 const queryClient = new QueryClient();
 
@@ -11,7 +12,9 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <Toaster position="top-center" richColors />
-      <App />
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <App />
+      </ThemeProvider>
     </QueryClientProvider>
   </StrictMode>
 );
